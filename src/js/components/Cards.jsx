2f1eec8a3880d3691f3cac2fe3523ce282b9e553
Oscar Andres Bolaños Chacon <totmom07@gmail.com>
2025-03-11 04:00:06 +0000

    Correccion

--- src/js/components/Cards.jsx
+++ src/js/components/Cards.jsx
@@ -3,52 +3,52 @@ import React from 'react';
 const Cards = () => {
     return (
         <div className="container p-0">
-            <div class="row row-cols-1 row-cols-md-4 g-4">
-                <div class="col">
-                    <div class="card">
-                        <img src="https://pintemax.com/assets/images/blog_single_img1.jpg" class="card-img-top" alt="..." />
-                        <div class="card-body text-center p-5">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <div className="row row-cols-1 row-cols-md-4 g-4">
+                <div className="col">
+                    <div className="card">
+                        <img src="https://pintemax.com/assets/images/blog_single_img1.jpg" className="card-img-top" alt="..." />
+                        <div className="card-body text-center p-5">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                         </div>
                         <div className="card-footer text-center">
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                            <a href="#" className="btn btn-primary">Go somewhere</a>
                         </div>
                     </div>
                 </div>
-                <div class="col">
-                    <div class="card">
-                        <img src="https://pintemax.com/assets/images/blog_single_img1.jpg" class="card-img-top" alt="..." />
-                        <div class="card-body text-center p-5">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <div className="col">
+                    <div className="card">
+                        <img src="https://pintemax.com/assets/images/blog_single_img1.jpg" className="card-img-top" alt="..." />
+                        <div className="card-body text-center p-5">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                         </div>
                         <div className="card-footer text-center">
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                            <a href="#" className="btn btn-primary">Go somewhere</a>
                         </div>
                     </div>
                 </div>
-                <div class="col">
-                    <div class="card">
-                        <img src="https://pintemax.com/assets/images/blog_single_img1.jpg" class="card-img-top" alt="..." />
-                        <div class="card-body text-center p-5">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <div className="col">
+                    <div className="card">
+                        <img src="https://pintemax.com/assets/images/blog_single_img1.jpg" className="card-img-top" alt="..." />
+                        <div className="card-body text-center p-5">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                         </div>
                         <div className="card-footer text-center">
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                            <a href="#" className="btn btn-primary">Go somewhere</a>
                         </div>
                     </div>
                 </div>
-                <div class="col">
-                    <div class="card">
-                        <img src="https://pintemax.com/assets/images/blog_single_img1.jpg" class="card-img-top" alt="..." />
-                        <div class="card-body text-center p-5">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <div className="col">
+                    <div className="card">
+                        <img src="https://pintemax.com/assets/images/blog_single_img1.jpg" className="card-img-top" alt="..." />
+                        <div className="card-body text-center p-5">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                         </div>
                         <div className="card-footer text-center">
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                            <a href="#" className="btn btn-primary">Go somewhere</a>
                         </div>
                     </div>
                 </div>
